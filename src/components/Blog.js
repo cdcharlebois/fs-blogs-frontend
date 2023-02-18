4711ccaca00fbcc3d0cf2d11b461ed3c276df73e
Conner Charlebois <cdcharlebois@gmail.com>
2023-02-18 13:50:46 -0500

    5.7 - show and hide blog details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,17 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  return (
+    <div>
+      <h3>{blog.title} <button onClick={() => setShowDetails(!showDetails)}>{showDetails ? "hide" : "show"} details</button></h3>
+      {showDetails ? <div>
+        <div>Author: {blog.author}</div>
+        <div>Likes: {blog.likes}</div>
+        <div>URL: {blog.url}</div>
+      </div> : null}
+    </div>
+  );
+};
+
+export default Blog;
